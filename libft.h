64afa4fed1136eefc341a27d0b9df8d2b231e8dc
Victor Elbling <vielblin@k1r4p5.42mulhouse.fr>
2024-10-16 20:14:04 +0200

    cloud

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vielblin <vielblin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:18:02 by vielblin          #+#    #+#             */
-/*   Updated: 2024/10/16 18:32:47 by vielblin         ###   ########.fr       */
+/*   Updated: 2024/10/16 20:11:31 by vielblin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
